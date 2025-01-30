986b93ef5ea2a8e3b548a4bf7c9fa36d0a6823f2
24LP1447 24LP1447 <24LP1447@G303-CC0161.local>
2025-01-30 12:45:28 +0800

    starter

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,82 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+function FruitsContainer(props){
+  return( <div className="fruitsContainer">
+    <CategoryName dataCategory={props.dataCategory}></CategoryName>
+    <Items></Items>
+    <Items></Items>
+    <Items></Items>
+  </div>)
+}
+function CategoryName(props){
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+   <div className="categoryName">
+    <h1>{props.dataCategory}</h1>
+   </div>
+  )
+}
+function Items(props){
+  return(
+    <div className="fruitsItems">
+      <div className="containerMain">
+        <p>Name</p>
+        <p>Price</p>
+      </div>
     </div>
-  );
+  )
+}
+function SearchItems(props){
+  return (
+    <div className="details">
+    <input placeholder="Search..."/>
+    <div className="detailsA">
+      <input type="checkbox"/>
+      <p>Ony show products in stock</p>
+    </div>
+  </div>
+  )
+}
+function Title(props){
+  return (
+    <div className="title">
+    <div className="details">
+      <h1>Name</h1>
+      <h1>Price</h1>
+    </div>
+   </div>
+  )
+}
+
+const SIMPLE_DATA = [
+  {
+    categoryName: "Fruits",
+    items: []
+  },
+  {
+    categoryName: "Fruits",
+    items: []
+  }
+]
+
+const DATA = [  { category: "Fruits", price: "$1", stocked: true, name: "Apple" }, 
+   { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },  
+   { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },  
+   { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },  
+   { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" }, 
+   { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }]
+
+export default function Home() {
+
+ return ( <div className="bodyContainer">
+   <div className="container">
+      <div className="search">
+       <SearchItems/>
+      </div>
+      <Title/>
+      <FruitsContainer dataCategory = {DATA[0].category}/>
+      
+   </div>
+
+</div>)
 }
