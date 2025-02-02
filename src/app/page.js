652658e923ff1e81fdccd841f83b838e966e8a8f
Oyunbat <oyunbat9958@gmail.com>
2025-02-02 01:59:03 -0600

    map loop is done

--- src/app/page.js
+++ src/app/page.js
@@ -1,53 +1,50 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-function FruitsContainer(props){
+function FruitsContainer(props) {
+  const ItemContainer = [];
+  props.dataName.items.map((el) => {
+    if (props.dataName.items.length) {
+      ItemContainer.push(<Items itemData={el}></Items>)
+    }
+  })
 
-  const containerItem = [];
-  for(let i = 0 ; i < props.dataName.items.length;i++){
-  containerItem.push(<Items itemData = {props.dataName.items[i]}></Items>)
-  }
-
-  return( <div className="fruitsContainer">
-    <CategoryName dataCateName = {props.dataName.categoryName}></CategoryName>
-    {containerItem}
+  return (<div className="fruitsContainer">
+    <CategoryName dataCateName={props.dataName.categoryName}></CategoryName>
+    {ItemContainer}
   </div>)
 }
-function CategoryName(props){
+function CategoryName(props) {
   return (
-   <div className="categoryName">
-    <h1>{props.dataCateName}</h1>
-   </div>
+    <div className="categoryName">
+      <h1>{props.dataCateName}</h1>
+    </div>
   )
 }
-function SearchItems(props){
+function SearchItems(props) {
   return (
     <div className="details">
-    <input placeholder="Search..."/>
-    <div className="detailsA">
-      <input type="checkbox"/>
-      <p>Only show products in stock</p>
+      <input placeholder="Search..." />
+      <div className="detailsA">
+        <input type="checkbox" />
+        <p>Only show products in stock</p>
+      </div>
     </div>
-  </div>
   )
 }
-
-
-
-
-function Title(props){
+function Title(props) {
   return (
     <div className="title">
-    <div className="details">
-      <h1>Name</h1>
-      <h1>Price</h1>
+      <div className="details">
+        <h1>Name</h1>
+        <h1>Price</h1>
+      </div>
     </div>
-   </div>
   )
 }
-function Items(props){
+function Items(props) {
 
-  return(
+  return (
     <div className="fruitsItems">
       <div className="containerMain">
         <p>{props.itemData.name}</p>
@@ -60,63 +57,54 @@ const SIMPLE_DATA = [
   {
     categoryName: "Fruits",
     items: [
-        {
-          name: 'Apple',
-          price : '$1'
-        },
-        {
-          name: 'Dragonfruit',
-          price : '$1'
-            },
-        {
-          name: 'Passionfruit',
-          price : '$2'
-        }
+      {
+        name: 'Apple',
+        price: '$1'
+      },
+      {
+        name: 'Dragonfruit',
+        price: '$1'
+      },
+      {
+        name: 'Passionfruit',
+        price: '$2'
+      }
     ]
   },
   {
     categoryName: "Vegetables",
     items: [
-        {
-          name: 'Spinach',
-          price : '$2'
-        },
-        {
-          name: 'Pumpkin',
-          price : '$4'
-            },
-        {
-          name: 'Peas',
-          price : '$1'
-        }
+      {
+        name: 'Spinach',
+        price: '$2'
+      },
+      {
+        name: 'Pumpkin',
+        price: '$4'
+      },
+      {
+        name: 'Peas',
+        price: '$1'
+      }
     ]
   },
 
 ]
-
-
-
-
 export default function Home() {
-
-  const containerCategories = [];
-
+  const ItemsContainer = [];
   SIMPLE_DATA.map((el) => {
-  console.log(el)
+    if (SIMPLE_DATA.length == SIMPLE_DATA.length) {
+      ItemsContainer.push(<FruitsContainer dataName={el}></FruitsContainer>)
+    }
   });
 
-  for(let i = 0 ; i < SIMPLE_DATA.length ; i++){
-    containerCategories.push(<FruitsContainer dataName = {SIMPLE_DATA[i]}></FruitsContainer>)
-  }
-
- return ( <div className="bodyContainer">
-   <div className="container">
+  return (<div className="bodyContainer">
+    <div className="container">
       <div className="search">
-       <SearchItems/>
+        <SearchItems />
       </div>
-      <Title/>
-      {containerCategories}
-   </div>
-
-</div>);
+      <Title />
+      {ItemsContainer}
+    </div>
+  </div>);
 }
